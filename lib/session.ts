625135ba7ff3e0a5301d16ce394a3c8c8fe715cf
George Koloidi <Battleaxe@bk.ru>
2023-06-30 22:25:56 +0200

    feat: add action and graphql request

--- lib/session.ts
+++ lib/session.ts
@@ -4,14 +4,15 @@ import { Adapter, AdapterUser } from 'next-auth/adapters';
 import GoogleProvider from 'next-auth/providers/google';
 import jsonwebtoken from 'jsonwebtoken';
 import { JWT } from 'next-auth/jwt';
-import { SessionInterface } from '@/common.type';
+import { SessionInterface, UserProfile } from '@/common.type';
+import { createUser, getUser } from './actions';
 
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    })
+    }),
   ],
   // jwt: {
   //   encode: ({secret, token}) => {
@@ -23,26 +24,47 @@ export const authOptions: NextAuthOptions = {
   // },
   theme: {
     colorScheme: 'light',
-    logo: '/logo.png'
+    logo: '/logo.png',
   },
   callbacks: {
     async session({ session }) {
-      return session;
-    },
-    async signIn({ user }: {user: AdapterUser | User}) {
+      const email = session?.user?.email as string;
+
       try {
+        const data = (await getUser(email)) as { user?: UserProfile };
 
+        const newSession = {
+          ...session,
+          user: {
+            ...session.user,
+            ...data?.user,
+          },
+        };
 
-        return true
+        return newSession;
       } catch (error) {
-        console.log(error)
-        return false
+        console.log('Error retrieving user data', error);
+        return session;
       }
-    }
-  }
-}
+    },
+    async signIn({ user }: { user: AdapterUser | User }) {
+      try {
+        const userExists = (await getUser(user?.email as string)) as { user?: UserProfile };
+
+        if (!userExists) {
+          await createUser(user.name as string, user.email as string, user.image as string);
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
+};
 
 export async function getCurrentUser() {
-  const session = await getServerSession(authOptions) as SessionInterface;
-  return session
+  const session = (await getServerSession(authOptions)) as SessionInterface;
+  return session;
 }

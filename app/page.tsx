c091091e8e2f24a4c5e71dc4bc30f7aab057d9e0
George Koloidi <Battleaxe@bk.ru>
2023-06-30 20:34:16 +0200

    feat: Navbar component basic skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <section className='flex-start flex-col paddings  mb-16'>
+      <h2>Categories</h2>
+      <h2>Posts</h2>
+      <h2>LoadMore</h2>
+    </section>
   );
 }

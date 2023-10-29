12078109d1068ac23c1bff85efd7a79683b81f9d
George Koloidi <Battleaxe@bk.ru>
2023-10-29 21:06:05 +0100

    fix: add empty stirng for categories graphql

--- graphql/index.ts
+++ graphql/index.ts
@@ -37,7 +37,7 @@ export const deleteProjectMutation = `
     }
   }
 `;
-      
+
 export const createUserMutation = `
 	mutation CreateUser($input: UserCreateInput!) {
 		userCreate(input: $input) {
@@ -55,7 +55,7 @@ export const createUserMutation = `
 `;
 
 export const projectsQuery = `
-  query getProjects($category: String, $endcursor: String) {
+    query getProjects($category: String = "", $endcursor: String) {
     projectSearch(first: 8, after: $endcursor, filter: {category: {eq: $category}}) {
       pageInfo {
         hasNextPage
@@ -117,7 +117,7 @@ export const getUserQuery = `
     }
   }
 `;
-      
+
 export const getProjectsOfUserQuery = `
   query getUserProjects($id: ID!, $last: Int = 4) {
     user(by: { id: $id }) {
